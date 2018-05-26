2e2de9e48d2b0311b43ca6bbd8b1c2b35bca6d71
Sricharan <sricharanized@gmail.com>
2018-05-26 11:24:43 +0200

    annotated bagging

--- get_tags.py
+++ get_tags.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import utils as datautils
 import operator
 import cPickle
 import itertools
 import pandas as pd
+import json
 
+from eventregistry import *
 from document_clean import Document
 from pprint import pprint
 from preprocess import TokenProcessor
 from term_freq import TFIDF
+
 
 class Tagger:
     def __init__(self):
@@ -46,6 +49,10 @@ class Tagger:
     def __str__(self):
         return str(pprint(vars(self)))
 
+
+
+
+
 def test_article(tagDict,i):
     token_processor = TokenProcessor()
     testfile = "Similarity/workfile.txt"
@@ -74,7 +81,23 @@ def main():
     for i in range(25):
         tagDict=test_article(tagDict,i)
 
+    testfile = "Similarity/workfile.txt"
+    token_processor = TokenProcessor()
+    er = EventRegistry(apiKey = 'ab40eb06-3900-4689-a369-b4098f4e49ef')
+    # doc = datautils.get_test_document(testfile, token_processor)
+
+    file1 = open(testfile,'r')
+    text = file1.read()
+
+    analytics = Analytics(er)
+    ann = analytics.annotate(text)
+    annotations = []
+    parsed_json = json.loads(json.dumps(ann))
+    for annotation in parsed_json[u'annotations']:
+      annotations.append(annotation[u'title'])
+    # print(annotations)
     my_list= list(itertools.chain(*tagDict.values()))
+    my_list = my_list + annotations
     print dict(pd.value_counts(my_list))
     
 if __name__ == "__main__":
--- playground.py
+++ playground.py
@@ -1,23 +1,37 @@
 # from summa.summarizer import summarize
+import json
 
-text = """Automatische Zusammenfassung ist der Prozess zum Reduzieren eines Textdokuments mit einem
-Computerprogramm, um eine Zusammenfassung zu erstellen, die die wichtigsten Punkte enthalt
-des Originaldokuments. Da das Problem der Informationsuberlastung gewachsen ist, und wie
-die Datenmenge hat zugenommen, hat also Interesse an der automatischen Zusammenfassung.
-Technologien, die eine koharente Zusammenfassung machen konnen, berucksichtigen Variablen wie
-Lange, Schreibstil und Syntax. Ein Beispiel fur die Verwendung der Zusammenfassungstechnologie
-ist Suchmaschinen wie Google. Dokumentenzusammenfassung ist eine andere."""
+from eventregistry import *
+er = EventRegistry(apiKey = 'ab40eb06-3900-4689-a369-b4098f4e49ef')
 
-# print(summarize(text, language='german'))
 
-from rake_nltk import Rake
+text = """
+Mit Beginn der Tail Emission von 157.788 XMR p. a. ab ca. Mitte 2022 bei einer dann emittierten Geldmenge von 18,132 Millionen XMR liegt das nominale Geldmengenwachstum bei anfänglich 0,87 % p. a., wird in der Folge aufgrund der dauernd steigenden Gesamtzahl emittierten Geldes aber kontinuierlich fallen und langfristig gegen 0 % konvergieren.
 
-# Uses stopwords for english from NLTK, and all puntuation characters.
-# r = Rake()
-r = Rake(language="german")
+Da benutzerbedingt immer wieder Moneros verloren gehen werden (Verlust privater Schlüssel, Hardware-Defekte, fehlende Backups), könnte sich durch den Mechanismus der Tail Emission langfristig ein ungefähres Gleichgewicht zwischen der Rate verlorener und neu erzeugter Coins einstellen. Das bedeutet, dass Monero trotz der dauerhaften Erzeugung neuer Währungseinheiten langfristig als Währung mit stabiler Geldmenge angesehen werden könnte.
+Anonymität und Datenschutz
+"""
 
-print(r.extract_keywords_from_text(text))
 
-r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
+analytics = Analytics(er)
+ann = analytics.annotate(text)
+annotations = []
+parsed_json = json.loads(json.dumps(ann))
+for annotation in parsed_json[u'annotations']:
+  annotations.append(annotation[u'title'])
 
-print(r.get_ranked_phrases())
+print(annotations)
+
+# # print(summarize(text, language='german'))
+
+# from rake_nltk import Rake
+
+# # Uses stopwords for english from NLTK, and all puntuation characters.
+# # r = Rake()
+# r = Rake(language="german")
+
+# print(r.extract_keywords_from_text(text))
+
+# r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
+
+# print(r.get_ranked_phrases())
